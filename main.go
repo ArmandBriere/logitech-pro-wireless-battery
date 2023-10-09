53673cb002266ed2d3381bf35f8920bb22b70bf7
Armand Briere <armand.123@live.fr>
2023-10-09 12:41:53 -0400

    feat: for loop and default notification system

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -10,8 +14,17 @@ import (
 const (
 	PercentageWarning = 30
 	PercentageEmpty   = 10
-	PollingInterval   = 3
+	PollingInterval   = 5
+
+	notificationTimeout = "5000"
+
+	BATTERY_FULL_PNG  = "./icons/battery-full.png"
+	BATTERY_HALF_PNG  = "./icons/battery-half.png"
+	BATTERY_EMPTY_PNG = "./icons/battery-empty.png"
 )
+
+var headsetName string
+var batteryStatus int
 
 // setLogger and logger level
 func setLogger() {
@@ -25,8 +38,52 @@ func setLogger() {
 	log.SetOutput(os.Stdout)
 }
 
+// Run headsetcontrol binary to get headset info
+func execHeadsetcontrol() {
+	output, err := exec.Command("/usr/bin/headsetcontrol", "-b").Output()
+	if err != nil {
+		log.Debug("Headset not found.")
+		return
+	}
+
+	log.Debug(string(output))
+
+	headsetFoundSplit := strings.Split(string(output), "Found ")
+
+	if len(strings.Split(string(output), "Found ")) == 1 {
+		log.Debug("Headset not found.")
+		return
+	}
+
+	headsetName = strings.Split(headsetFoundSplit[1], "!")[0]
+	headsetBatterySplit := strings.Split(string(output), "Battery: ")
+	if len(headsetBatterySplit) == 1 {
+		log.Debug("Headset usb is connected but the headset is in sleep mode.")
+		return
+	}
+
+	batteryStatus, _ = strconv.Atoi(strings.Split(headsetBatterySplit[1], "%")[0])
+	log.Debug("Headset name: " + headsetName)
+	log.Debug("Battery status: " + strconv.Itoa(batteryStatus))
+
+	sendNotification()
+}
+
+func sendNotification() {
+
+	message := headsetName + " - Battery at " + strconv.Itoa(batteryStatus) + "%"
+
+	_, err := exec.Command("/usr/bin/notify-send", "-t", notificationTimeout, message).Output()
+	if err != nil {
+		log.Error(err.Error())
+	}
+}
+
 func main() {
 	setLogger()
 
-	log.Debug("hi")
+	for {
+		execHeadsetcontrol()
+		time.Sleep(PollingInterval * time.Second)
+	}
 }
